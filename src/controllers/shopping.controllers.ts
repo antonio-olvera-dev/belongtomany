f656d84a1c86f98f337800f54461048d4cd38a4e
PC-ANTONIO\toni_ <antonioolvera1995@gmail.com>
2020-11-24 14:37:23 +0100

    *V0*

---
+++ src/controllers/shopping.controllers.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from "express";
+import { Clients } from "../models/clients.model";
+import { Products } from "../models/products.model";
+import { Shopings } from "../models/shopings.model";
+
+
+
+class ShoppingController {
+
+
+  public async getAll(req: Request, res: Response) {
+
+    try {
+
+
+      
+
+      const shop = await Clients.findAll({
+
+        include: Products
+
+      });
+
+      res.send(shop);
+
+    } catch (error) {
+      console.log(error);
+      res.send(error);
+    }
+
+  }
+
+
+  public async setProducts(req: Request, res: Response) {
+
+    try {
+
+
+      
+
+      const shop = await Shopings.create(req.body);
+
+      res.send(shop);
+
+    } catch (error) {
+
+      console.log(error);
+      res.send(error);
+    }
+
+  }
+
+
+
+
+
+
+}
+
+export const shoppingController = new ShoppingController();
